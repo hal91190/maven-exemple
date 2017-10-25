e3fde02110846415dee13b46e91b89d5d523bed0
Stéphane Lopes <stephane.lopes@uvsq.fr>
2017-10-25 17:37:12 +0200

    Implémente un test (création avec un montant invalide)

--- src/test/java/fr/uvsq/poo/compte/AccountTest.java
+++ src/test/java/fr/uvsq/poo/compte/AccountTest.java
@@ -11,15 +11,22 @@ import static org.hamcrest.core.IsEqual.equalTo;
 
 public class AccountTest {
   BigDecimal amount;
+  BigDecimal invalidAmount;
 
   @Before
   public void setup() {
     amount = new BigDecimal("100");
+    invalidAmount = new BigDecimal("-100");
   }
 
   @Test
   public void anAccountCreatedWithAnAmountShouldHaveABalanceEqualsToThisAmount() {
     Account account = new Account(amount);
     assertThat(account.getBalance(), is(equalTo(amount)));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void aCreationWithANegativeAmountShouldFail() {
+    Account account = new Account(invalidAmount);
   }
 }
