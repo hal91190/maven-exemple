7885322a83c96a847cba08749d7b5bb1474b908b
Stéphane Lopes <stephane.lopes@uvsq.fr>
2017-10-25 17:25:38 +0200

    Fait en sorte que le test compile... et échoue

---
+++ src/main/java/fr/uvsq/poo/compte/Account.java
@@ -0,0 +1,18 @@
+package fr.uvsq.poo.compte;
+
+import java.math.BigDecimal;
+
+/**
+ * La classe <code>Account</code> représente un compte bancaire.
+ *
+ * @author Stéphane Lopes
+ * @version 2017
+ */
+class Account {
+  public Account(BigDecimal initialBalance) {
+  }
+
+  public BigDecimal getBalance() {
+    return BigDecimal.ZERO;
+  }
+}
